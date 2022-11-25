9ce390b6032911d5246b1bf57b706dce2b6329ff
Rane-school <ksrg.z100@gmail.com>
2022-11-25 12:30:11 +0900

    tested fit_and_score function in SearchCV #25

--- modules/model_selection/_search.py
+++ modules/model_selection/_search.py
@@ -5,8 +5,9 @@ from itertools import product
 from sklearn.model_selection import TimeSeriesSplit
 import numpy as np
 
-from metrics import get_scorer
-from decoder import RidgeRegression
+from modules.metrics import get_scorer
+from modules.decoder import RidgeRegression
+from ._split import RollingOriginSplit
 
 @dataclass
 class SearchCV:
@@ -14,16 +15,17 @@ class SearchCV:
     estimator: RidgeRegression
     scoring: str
     candidate_params: list
-    cv: TimeSeriesSplit
+    n_split : int
 
     def __post_init__(self) -> None:
         """Post processing."""
         self.scorer = get_scorer(self.scoring)
+        self.cv = RollingOriginSplit(self.n_split)
 
-    def fit_and_score(self, X, y, train_index, test_index, hyper_param) -> dict:
+    def fit_and_score(self, X, y, train_indexes, test_indexes, hyper_param) -> dict:
         """Fit estimator and compute scores for a given dataset split."""
-        X_train, X_test = X[train_index], X[test_index]
-        y_train, y_test = y[train_index], y[test_index]
+        X_train, X_test = X[train_indexes], X[test_indexes]
+        y_train, y_test = y[train_indexes], y[test_indexes]
 
         estimator = deepcopy(self.estimator)
 
@@ -31,25 +33,22 @@ class SearchCV:
         fitted_param = estimator.fitted_param
 
         estimator.predict(X_test)
-
         y_pred = estimator.prediction
 
         result = {
             "train_scores": self.scorer(y_train, np.einsum("ij,j->i",X_train, fitted_param)),
             "test_scores" : self.scorer(y_test, y_pred),
-            "fitted_parameters": fitted_param,
+            "fitted_param": fitted_param,
         }
 
         return result
     
-    def evaluate_candidates(self, X, y, n_split):
+    def evaluate_candidates(self, X, y):
         """Run search among cv splits and get the best parameters."""
         self.results = dict()
         min_score = np.inf
-        for id_, param, (train_index, test_index) in enumerate(product(self.candidate_params,self.cv(n_split = n_split).split(X))):
-            result = self.fit_and_score(X, y, train_index, test_index, param)
-
-            # evaluate the the RMSE in one cv session !!
+        for id_, param, (train_indexes, test_indexes) in enumerate(product(self.candidate_params,self.cv.split(X))):
+            result = self.fit_and_score(X, y, train_indexes, test_indexes, param)
 
             self.results[id_] = {
                 "fit_and_score": result,
@@ -59,5 +58,3 @@ class SearchCV:
                 min_score = result["test_scores"]
                 self.best_result = self.results[id_]
                 
-
-    
