d142e51649a34ab7c750491fbd01426b441a30dd
Rane-school <ksrg.z100@gmail.com>
2022-11-30 10:05:26 +0900

    WIP: train with only past coord #27

--- modules/dataloader/dataset.py
+++ modules/dataloader/dataset.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from functools import cached_property
 from pathlib import Path
 from typing import Tuple
 import numpy as np
@@ -14,7 +15,7 @@ def _is_valid_axis(coord_axis: str) -> bool:
     return False
 
 @dataclass
-class PastCoordDataset:
+class SpikesPastCoordDataset:
     """Dataset that includes one mouse's spikes and coordinates.
     
     This dataset is for regression model that incorporates past 
@@ -86,6 +87,78 @@ class PastCoordDataset:
 
         return coords,spikes
 
+
+@dataclass
+class PastCoordDataset:
+    """Dataset that includes one mouse's spikes and coordinates.
+    
+    This dataset is for regression model that incorporates past 
+    coordinates as one of the features in design matrix.
+
+    Parameters
+    ---------
+    datadir : Path
+        the path to a mouse's data
+    coord_axis : str
+        which axis to use
+    nthist : int
+        the number of history bins
+    
+    """
+    data_dir : Path
+    coord_axis : str
+    nthist : int
+
+    def __post_init__(self) -> None:
+        """Post precessing."""
+        if not _is_valid_axis(self.coord_axis):
+            raise ValueError("The coord_axis can either be 'x-axis' or 'y-axis'.")
+        self.axis = 0 if self.coord_axis == "x-axis" else 1
+        self.data = self.load_all_data()
+
+    @property
+    def design_matrix(self) -> np.array:
+        """Make design matrix for decoder with past corrdinates.
+
+        Parameter:
+        ----------
+        spikes: np.array
+            that has neurons's spikes count data.
+        coordinates: np.array
+            x or y coordinate data
+        nthist: int
+            num of time bins for spikes history
+        """
+        n_time_bins, _ = self.spikes.shape
+        if self.nthist !=0:
+            design_m = self.coord[:-self.nthist]
+        else:
+            raise ValueError("nthist must be larger than 0.")
+
+        design_mat_all_offset = np.hstack((np.ones((n_time_bins-self.nthist,1)), design_m))
+        return design_mat_all_offset
+
+    def load_all_data(self) -> Tuple[np.array, np.array]:
+        """Load design matrix and corresponding response(coordinate)."""
+        coords_xy, self.spikes = self._load_data()
+        self.coord = coords_xy[:,self.axis]
+        return self.design_matrix, self.coord[self.nthist:]
+
+    @cached_property
+    def _load_data(self) -> Tuple[np.array, np.array]:
+        """Load coordinates and spike data."""
+        coords_df = pd.read_excel(self.data_dir/'position.xlsx')
+        coords=coords_df.values[3:,1:3] # only take the X,Y axis data
+
+        spikes_df = pd.read_excel(self.data_dir/'traces.xlsx',index_col=0)
+        spikes = spikes_df.values
+
+        # make sure spike and postion data have the same length
+        n_bins = min(len(coords),len(spikes))
+        coords = coords[:n_bins]
+        spikes = spikes[:n_bins]
+
+        return coords,spikes
     
 
 
---
+++ notebooks/03-past-coordinates/02-implement-only-past-coord/train.py
@@ -0,0 +1,51 @@
+from modules.func import *
+from modules.decoder import RidgeRegression
+from tqdm import tqdm
+import pickle
+from itertools import product
+from typing import Tuple
+
+from param import *
+from modules.dataloader import PastCoordDataset
+from modules.model_selection import SearchCV
+
+def spilt_data(X: np.array, y: np.array, train_ratio: float) -> Tuple[Tuple[np.array,np.array],Tuple[np.array,np.array]]:
+    """Get training and testing data."""
+    train_size = int( len(X) * train_ratio )
+    X_train,y_train = X[:train_size], y[:train_size]
+    X_test,y_test = X[train_size:], y[train_size:]
+    return (X_train, y_train), (X_test, y_test)
+
+
+def main() -> None:
+    # coordinate
+    coord_axis_opts=["x-axis","y-axis"]
+
+    # data dir
+    datalist = ParamDir().data_path_list
+
+    # output dir
+    output_dir = ParamDir().OUTPUT_DIR
+    if not output_dir.exists():
+        output_dir.mkdir()
+
+    # get the regression results for all the mice
+    for data_dir in tqdm(datalist[[0,2]]):
+        data_name = str(data_dir).split('/')[-1]
+
+        results_all=[]
+        for nthist, coord_axis in product(ParamData().nthist_range, coord_axis_opts):
+            design_matrix, coord = PastCoordDataset(data_dir, coord_axis, nthist).data # load coordinates and spikes data
+
+            (X_train, y_train), (_, _) = spilt_data(design_matrix, coord, .8)
+
+            search = SearchCV(RidgeRegression(), ParamTrain().scoring, ParamTrain().penalty_range, ParamTrain().n_split)
+            search.evaluate_candidates(X_train, y_train)
+            results_all.append((search.best_result, nthist, coord_axis))
+
+        # ---save results
+        with open(output_dir/(f"rr_past_coord_{data_name}.pickle"),"wb") as f:
+            pickle.dump(results_all,f)
+
+if __name__ == "__main__":
+    main()
