4b23568a991cf12d1b3e1cf268c4a0662ea712ad
Rane-school <ksrg.z100@gmail.com>
2022-12-01 17:33:41 +0900

    implement eval script without cv #27

--- notebooks/03-past-coordinates/02-implement-only-past-coord/eval.py
+++ notebooks/03-past-coordinates/02-implement-only-past-coord/eval.py
@@ -11,26 +11,31 @@ def main():
     output_dir = ParamDir().output_dir
     datalist = ParamDir().data_path_list
 
-    coord_axis = "x-axis"
 
     for data_dir in tqdm(datalist[[0,2]]):
         data_name = str(data_dir).split('/')[-1]
-        with open(output_dir/(f"rr_only_past_coord_{data_name}_{coord_axis}.pickle"),"rb") as f:
+        dataset = PastCoordDataset(data_dir)
+        with open(output_dir/(f"rr_only_past_coord_{data_name}.pickle"),"rb") as f:
             results_all = pickle.load(f)
 
         eval_results_all = []
         for result in results_all:
-            best_result, nthist, coord_axis = result
-            design_matrix, coord = PastCoordDataset(data_dir, coord_axis, nthist).data # load coordinates and spikes data
+            design_matrix, coord = dataset.load_all_data(result["coord_axis"], result["nthist"]) # load coordinates and spikes data
 
             (_, _), (X_test, y_test) = spilt_data(design_matrix, coord, ParamTrain().train_size)
 
             rr = RidgeRegression()
-            rr.load(best_result["fitted_param"])
+            rr.load(result["fitted_param"])
             test_scores, y_pred = rr.evaluate(X_test, y_test, ParamEval().scoring)
-            eval_results_all.append((nthist, coord_axis, best_result, test_scores, y_pred, y_test))
+            eval_result = {
+                "eval_test_scores" : test_scores,
+                "y_pred": y_pred,
+                "y_test": y_test
+            }
+            eval_result.update(result)
+            eval_results_all.append((eval_result))
 
-            with open(output_dir/(f"rr_only_past_coord_eval_{data_name}_{coord_axis}.pickle"),"wb") as f:
+            with open(output_dir/(f"rr_only_past_coord_eval_{data_name}.pickle"),"wb") as f:
                 pickle.dump(eval_results_all,f)
 
 if __name__ == "__main__":
