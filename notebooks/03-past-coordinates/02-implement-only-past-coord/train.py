c357dce1bb20893a8e48e50f994523fe2843e7d1
Rane-school <ksrg.z100@gmail.com>
2022-12-01 15:13:13 +0900

    implement, eval and analyze #27

--- notebooks/03-past-coordinates/02-implement-only-past-coord/train.py
+++ notebooks/03-past-coordinates/02-implement-only-past-coord/train.py
@@ -19,32 +19,31 @@ def spilt_data(X: np.array, y: np.array, train_ratio: float) -> Tuple[Tuple[np.a
 
 def main() -> None:
     # coordinate
-    coord_axis_opts=["x-axis","y-axis"]
+    # coord_axis_opts=["x-axis",]
+    coord_axis = "x-axis"
 
     # data dir
     datalist = ParamDir().data_path_list
 
     # output dir
-    output_dir = ParamDir().OUTPUT_DIR
-    if not output_dir.exists():
-        output_dir.mkdir()
+    output_dir = ParamDir().output_dir
 
     # get the regression results for all the mice
     for data_dir in tqdm(datalist[[0,2]]):
         data_name = str(data_dir).split('/')[-1]
 
         results_all=[]
-        for nthist, coord_axis in product(ParamData().nthist_range, coord_axis_opts):
+        for nthist in tqdm(ParamData().nthist_range):
             design_matrix, coord = PastCoordDataset(data_dir, coord_axis, nthist).data # load coordinates and spikes data
 
             (X_train, y_train), (_, _) = spilt_data(design_matrix, coord, .8)
 
-            search = SearchCV(RidgeRegression(), ParamTrain().scoring, ParamTrain().penalty_range, ParamTrain().n_split)
+            search = SearchCV(ParamTrain().scoring, ParamTrain().penalty_range, ParamTrain().n_split)
             search.evaluate_candidates(X_train, y_train)
             results_all.append((search.best_result, nthist, coord_axis))
 
         # ---save results
-        with open(output_dir/(f"rr_past_coord_{data_name}.pickle"),"wb") as f:
+        with open(output_dir/(f"rr_only_past_coord_{data_name}_{coord_axis}.pickle"),"wb") as f:
             pickle.dump(results_all,f)
 
 if __name__ == "__main__":
