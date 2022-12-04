cd66e6e585f6a314ef88d43cc20db901b24c546a
Rane-school <ksrg.z100@gmail.com>
2022-12-04 15:22:38 +0900

    optimized the matrix cal #26

--- modules/decoder.py
+++ modules/decoder.py
@@ -32,20 +32,12 @@ class RidgeRegression():
         penalty: float
             the penalty added on ridge model
         '''
-        self.X_train=X_train
-        self.y_train=y_train.ravel()
-        self.penalty=penalty
+        self.X_train = X_train
+        self.y_train = y_train.ravel()
+        self.penalty = penalty
 
-        tmp1=np.einsum("ji,ik->jk",self.X_train.T,self.X_train)
-        tmp2=np.einsum("ji,i->j",self.X_train.T,self.y_train)
-        try: 
-            inv(tmp1 + penalty * np.identity(len(tmp1)))
-            self.fitted_param = np.einsum("ji,i->j",inv(tmp1+penalty*np.identity(len(tmp1))),tmp2)
-            self.fitting=True # indicate whether the fitting is successfully conducted
-
-        except: 
-            self.fitting = False
-            self.fitted_param = np.array([np.nan]*self.X_train.shape[1])
+        self.xxlam = inv(self.X_train.T @ self.X_train + penalty * np.identity(self.X_train.shape[1])) # (X'X + lambda*I)^-1
+        self.fitted_param = (self.xxlam @ self.X_train.T).dot(self.y_train)
 
     def predict(self, X_test: np.array):
         '''Predicting using fitted parameters based on test data.
@@ -58,7 +50,7 @@ class RidgeRegression():
             test design matrix including one column full of 1 for the intercept
 
         '''
-        self.prediction = np.einsum("ji,i->j", X_test, self.fitted_param)
+        self.prediction =  X_test @ self.fitted_param
 
     def load(self, fitted_param: np.array) -> None:
         self.fitted_param = fitted_param
@@ -67,7 +59,7 @@ class RidgeRegression():
         if self.fitted_param is None:
             raise ValueError("fitted parameters are not loaded, please call load() first.")
         else:
-            y_pred = np.einsum("ji,i->j", X_test, self.fitted_param)
+            y_pred = X_test @ self.fitted_param
             scorer = get_scorer(scoring)
             test_scores = scorer(y_test, y_pred)
             return test_scores, y_pred
