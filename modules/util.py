99e112fd6599aa4afdfc593af5215e282ed1a95a
Rane-school <ksrg.z100@gmail.com>
2022-12-15 02:09:42 +0000

    /usr/local/bin/python3 /work/notebooks/2-decoding-model/4-incorporate-history-spikes/2-summed-hist-spikes/train.py

--- modules/util.py
+++ modules/util.py
@@ -4,4 +4,5 @@ from numpy.typing import NDArray
 # ---------- kernel ---------
 def gauss(xx: NDArray, mu: float = 0, sigma: float = .2):
     """A Gaussian kernel."""
-    return 1 / ((2 * np.pi) ** 2 * sigma) * np.exp(- (xx - mu) ** 2 / (2 * sigma ** 2))
+    kernel = 1 / ((2 * np.pi) ** 2 * sigma) * np.exp(- (xx - mu) ** 2 / (2 * sigma ** 2))
+    return kernel / sum(kernel)
