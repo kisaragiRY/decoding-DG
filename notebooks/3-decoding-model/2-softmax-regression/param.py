af31e8cdae49f467650ca7fbead89b4e43e4e281
Rane-school <ksrg.z100@gmail.com>
2023-02-03 06:53:31 +0000

    visulaized the 4 grid classes #39

---
+++ notebooks/3-decoding-model/2-softmax-regression/param.py
@@ -0,0 +1,22 @@
+from typing import List
+
+from dataclasses import dataclass, field
+from pathlib import Path
+import numpy as np
+
+@dataclass
+class ParamDir:
+    """Param for directory."""
+    ROOT : Path = Path("/work")
+    DATA_ROOT : Path = ROOT/Path('data/alldata/')
+    OUTPUT_ROOT : Path = ROOT/Path("data/interim")
+
+    def __post_init__(self) -> None:
+        if not self.OUTPUT_ROOT.exists():
+            self.OUTPUT_ROOT.mkdir()
+
+        self.output_dir = self.OUTPUT_ROOT/"softmax_regression/"
+        if not self.output_dir.exists():
+            self.output_dir.mkdir()
+
+        self.data_path_list = np.array([x for x in self.DATA_ROOT.iterdir()])
