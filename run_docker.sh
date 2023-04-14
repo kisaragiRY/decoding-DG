ed2b49754c22198bf45029df0492f6d4b528831e
Rane-school <ksrg.z100@gmail.com>
2023-04-14 01:09:04 +0000

    start the docker with specified user

--- run_docker.sh
+++ run_docker.sh
@@ -1,5 +1,7 @@
-docker run -it \
-    --name dg-encoder-decoder\
+docker run --rm -it \
+    --name dg-encoder-decoder \
+    -u 202003:1000 \
     -v "$(pwd)":/work \
-    -v /home/zhang-r/.ssh:/root/.ssh\
-    dg-decoding:py3.8.5
+    -v /home/$USER/.ssh:/root/.ssh\
+    dg-decoding:py3.8.5 #\
+    # sh -c "groupadd -g $(id -g) dynamix && useradd -m -s /bin/bash -g $(id -g) -u $(id -u) $USER && usermod -a -G sudo $USER && su - $USER"
