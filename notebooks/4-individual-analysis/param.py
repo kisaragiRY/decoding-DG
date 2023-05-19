5323479f89a13bc25c9f86e4d9aa443de21a6eac
Rane-school <ksrg.z100@gmail.com>
2023-05-19 07:19:08 +0000

    added data param #64

--- notebooks/4-individual-analysis/param.py
+++ notebooks/4-individual-analysis/param.py
@@ -1,3 +1,4 @@
+from typing import Union
 from pathlib import Path
 import numpy as np
 
@@ -13,5 +14,16 @@ class ParamDir:
     def __post_init__(self) -> None:
         if not self.OUTPUT_ROOT.exists():
             self.OUTPUT_ROOT.mkdir()
-            
+
         self.data_list = np.array([x for x in self.DATA_ROOT.iterdir()])
+
+@dataclass
+class ParamData:
+    """Param for dataset.
+    """
+    window_size: int = 8
+    train_ratio: float = .8
+    mobility : float = 1.0
+    K : int = 16 # the threshold for the length of segments
+    shuffle: Union[bool, str] =  False # there are two methods: behavior shuffling, events shuffling and segment label shuffling
+    random_state : int = 202304
