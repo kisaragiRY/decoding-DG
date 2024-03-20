43b7178566742154ce9f2570334fdf504487653f
kisaragi <azeuunghy@gmail.com>
2024-03-20 03:44:57 +0000

    modified docker build setup #110

--- run_docker.sh
+++
@@ -1,6 +0,0 @@
-docker run --rm -it \
-    --name dg-encoder-decoder \
-    -u $(id -u):$(id -g) \
-    -v "$(pwd)":/work \
-    -v "$HOME/Documents/SH/Repos/Data/DG-imaging2-master/OF_Decoding Position, Speed, Motion Direction/data/alldata":/work/data/raw \
-    dg-decoding:py3.8.5 
